63bcb859a00d60a93e12f083802a1d1f10e8f410
Alona Puhach <alen1414@gmail.com>
2022-01-20 08:49:09 -0400

    add file

---
+++ src/index.js
@@ -0,0 +1,68 @@
+import './css/styles.css';
+const DEBOUNCE_DELAY = 300;
+
+const refs = {
+    formSearch: document.querySelector('#search-box'),
+    counnryList: document.querySelector('.country-list'),
+    countryInfo: document.querySelector('.country-info'),
+
+};
+console.log(refs.formSearch);
+console.log(refs.counnryList);
+console.log(refs.countryInfo);
+
+
+
+
+function httpInquiry (name) {
+    const SEARCH_URL = 'https://restcountries.com/v3.1/name/${name}';
+    const filter = '?fields=name,capital,population,flags,languages';
+    // const { name } = refs.formSearch.elements;
+    fetch('${SEARCH_URL}${filter}')
+        .then(response => {
+            return response.json();
+        })
+        .then(name => {
+            console.log(name);
+    
+        })
+        .catch(error => {
+            console.log(error);
+            
+        });
+   
+}
+ httpInquiry( )
+refs.formSearch.addEventListener('submit', onSearch)
+
+function onSearch(e) {
+    e.preventDefoult();
+    const form = e.currentTarget;
+    console.log(form.elements);
+};
+// function renderList(svg) {
+//     const items = []
+//     refs.counnryList.innerHTML = '';
+//     refs.counnryList.insertAdjacentHTML("beforebegin", items.join(''));
+// }
+
+function onSearch() {
+    
+};
+// function renderCard(name) {
+        
+//     }
+//  function httpInquiry(name) {
+//   const BASE_URL = 'https://restcountries.com/v3.1/name/';
+//   const filter = '?fields=name,capital,population,flags,languages';
+  
+//   return fetch(`${BASE_URL}${name}${filter}`)
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error(response.status)
+//       }
+//       return response.json()
+//     })
+//   }
+
+// httpInquiry()
