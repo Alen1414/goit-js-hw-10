c63891593bfdb1ca3b7cc45f37798aa4136bd4cd
Alona Puhach <alen1414@gmail.com>
2022-01-20 16:22:01 -0400

    error

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
 import cards from './cards.hbs';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const DEBOUNCE_DELAY = 300;
 
 const refs = {
@@ -11,26 +12,46 @@ const refs = {
 // console.log(refs.formSearch);
 // console.log(refs.counnryList);
 // console.log(refs.countryInfo);
-function httpInquiry (name) {
-    const SEARCH_URL = 'https://restcountries.com/v3.1/name/${name}';
-    const filter = '?fields=name,capital,population,flags,languages';
+
+function httpInquiry(name) {
+    const SEARCH_URL = `https://restcountries.com/v3.1/name/${name}`;
+    const  FILTER = '?fields=name,capital,population,flags,languages';
     // const { name } = refs.formSearch.elements;
-    fetch('${SEARCH_URL}${filter}')
+
+  return fetch(`${SEARCH_URL}${FILTER}`)
         .then(response => {
             return response.json();
         })
-        .then(name => {
-            console.log(name);
-            const markup = cards(name);
-            console.log(markup);
-        })
-        .catch(error => {
-            console.log(error);
-        });
+        };
+
    
-};
-httpInquiry()
  
+function renderCard(name) {
+//  console.log(name);
+ const markup = cards(name[0]);
+ // console.log(markup);
+ refs.countryInfo.innerHTML = markup;
+
+}
+
+
+refs.formSearch.addEventListener('input', onSearch)
+
+function onSearch(e) {
+    e.preventDefault()
+    const form = e.currentTarget;
+    console.log(form)
+    const serchQuuery = refs.formSearch.value;
+    console.log(serchQuuery);
+   
+    httpInquiry(serchQuuery)
+        .then(renderCard)
+        .catch(error => {
+            Notify.failure('Oops, there is no country with that name');
+            return error;
+        });
+}
+    // .finally(()=> form.reset())
 
 
 
@@ -39,16 +60,9 @@ httpInquiry()
 
 
 
-// refs.formSearch.addEventListener('submit', onSearch)
 
-// function onSearch(e) {
-//     e.preventDefoult();
-//     const form = e.currentTarget;
-//     const serchQuuery = form.elements.query.value;
 
-//     // console.log(form.elements);
-// httpInquiry (serchQuuery)
-// };
+
 
 
 // function renderList(svg) {
@@ -57,23 +71,4 @@ httpInquiry()
 //     refs.counnryList.insertAdjacentHTML("beforebegin", items.join(''));
 // }
 
-// function onSearch() {
-    
-// };
-// function renderCard(name) {
-        
-//     }
-//  function httpInquiry(name) {
-//   const BASE_URL = 'https://restcountries.com/v3.1/name/';
-//   const filter = '?fields=name,capital,population,flags,languages';
-  
-//   return fetch(`${BASE_URL}${name}${filter}`)
-//     .then(response => {
-//       if (!response.ok) {
-//         throw new Error(response.status)
-//       }
-//       return response.json()
-//     })
-//   }
 
-// httpInquiry()
