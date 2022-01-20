10b32784541c7be3004e658b3dafc1fdcce82790
Alona Puhach <alen1414@gmail.com>
2022-01-20 18:24:51 -0400

    dane

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './css/styles.css';
 // import Notiflix from 'notiflix';
 import cards from './cards.hbs';
+import shortCards from './sort-cards.hbs';
+
 const DEBOUNCE_DELAY = 300;
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from 'lodash.debounce'
@@ -24,20 +26,38 @@ function onSearch(e) {
     const form = e.currentTarget;
     console.log(form)
 const serchQuuery = refs.formSearch.value;
-console.log(serchQuuery);
+    console.log(serchQuuery);
+    
    
-   API.httpInquiry(serchQuuery)
+   API.httpInquiry(serchQuuery.trim())
         .then(renderCard)
         .catch(onFetchError)
     // .finally(()=> form.reset())
 };
 
 function renderCard(name) {
- console.log(name);
- const markup = cards(name[0]);
- // console.log(markup);
- refs.countryInfo.innerHTML = markup;
 
+    if (name.length > 10) {
+        Notify.info('"Too many matches found. Please enter a more specific name."');
+return 
+    };
+
+    if (name.length === 1) {
+       console.log(name);
+ const markup = cards(name[0]);
+ //
+        refs.countryInfo.innerHTML = markup;
+        return 
+    }
+
+    const markup = name.map(element => {return shortCards(element);
+    }).join('');
+ 
+    refs.countryInfo.innerHTML = markup;
+
+    console.log(name);
+     console.log(markup);
+    
 }
 function onFetchError(error) {
     Notify.failure('Oops, there is no country with that name');
